2b5cb6eebf22950acdfa8286d9a8b6c9d6f9005e
Lotte-Et <charlotte.stowe@gmail.com>
2024-04-20 14:16:43 -0400

    Starting to build out controllers for routes

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const { ObjectId } = require('mongoose').Types;
+const {User, Thought} = require('../models');
+
+module.exports = {
+    //GET all users
+    async getUsers(req, res){
+        try {
+            const users = await User.find();
+
+            const userObj = {
+                users
+            }
+            res.json(userObj)
+        }
+        catch (err){
+            return res.status(500).json(err);
+        }
+    },
+
+    //GET a single user
+    async getSingleUser(req, res){
+        try {
+            const user = await User.findOne({ _id: req.params.username}).select('-__v');
+            if (!user){
+                return res.status(404).json({ message: 'User not found'})
+            }
+            res.json({
+                user, 
+                thoughts: await thoughts(req.params.username)
+            })
+        }
+        catch (err){
+            return res.status(500).json(err);
+        }
+    }
+}
