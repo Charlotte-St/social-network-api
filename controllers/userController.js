2f7e4ea253233fc2324de1bed8dd36db2d10e65d
Lotte-Et <charlotte.stowe@gmail.com>
2024-04-21 14:14:54 -0400

    Fixed route to add friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,11 +59,12 @@ module.exports = {
     //DELETE User
     async deleteUser(req, res){
         try{
-            const user = await User.findOneAndDelete({ _id: req.params.username})
+            const user = await User.findOneAndDelete({ _id: req.params.userId})
 
             if (!user){
                 return res.status(404).json({message: 'User not found'})
             }
+            res.json({ message: 'User deleted'});
         }
         catch(err) {
             return res.status(500).json(err)
@@ -73,8 +74,8 @@ module.exports = {
     async addFriend(req, res){
         try {
             const user = await User.findOneAndUpdate(
-                { _id: req.params.username },
-                { $addToSet: { friends: req.body } }, 
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } }, 
                 { runValidators: true, new: true}
             );
             if (!user) {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,9 +9,7 @@ router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
 router.route('/:userId/thoughts').post(addThought);
 
-router.route('/:userId/friends').report(addFriend);
-
-router.route('/:userId/friends/:username').delete(removeFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 //Extra Credit: DELETE user and all their thoughts
 
